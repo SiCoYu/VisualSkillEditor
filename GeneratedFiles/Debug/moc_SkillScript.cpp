a3212b6f3d83b093a86cbd12b22e0001edc4e84d
riverlet-li <riverlet.li@gmail.com>
2020-03-07 03:26:24 +0800

    add gitignore

--- GeneratedFiles/Debug/moc_SkillScript.cpp
+++ GeneratedFiles/Debug/moc_SkillScript.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'SkillScript.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.6.3)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.8.0)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -13,12 +13,14 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'SkillScript.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.6.3. It"
+#error "This file was generated using the moc from 5.8.0. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
 
 QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_SkillScriptRefInfo_t {
     QByteArrayData data[1];
     char stringdata0[19];
@@ -375,4 +377,5 @@ void SkillScript::paramChanged(const SkillScriptNodeParam & _t1, SkillNotifyReas
     void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)), const_cast<void*>(reinterpret_cast<const void*>(&_t2)) };
     QMetaObject::activate(this, &staticMetaObject, 2, _a);
 }
+QT_WARNING_POP
 QT_END_MOC_NAMESPACE
--- GeneratedFiles/Release/moc_SkillGfx.cpp
+++ GeneratedFiles/Release/moc_SkillGfx.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'SkillGfx.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.6.3)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.8.0)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -13,12 +13,14 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'SkillGfx.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.6.3. It"
+#error "This file was generated using the moc from 5.8.0. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
 
 QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_SkillGfxData_t {
     QByteArrayData data[1];
     char stringdata0[13];
@@ -218,4 +220,5 @@ void SkillGfx::dataChanged(const SkillGfxData & _t1, SkillNotifyReason _t2)
     void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)), const_cast<void*>(reinterpret_cast<const void*>(&_t2)) };
     QMetaObject::activate(this, &staticMetaObject, 1, _a);
 }
+QT_WARNING_POP
 QT_END_MOC_NAMESPACE
